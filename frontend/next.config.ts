bdc564d7e4cf916bb9df80eaa761490abe03f5f2
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2025-03-22 17:27:24 -0400

    Allow images from all domains

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['**'],
   },
+  env: {
+    MEAL_SERVICE_DOMAIN: process.env.MEAL_SERVICE_DOMAIN,
+    MEAL_SERVICE_PORT: process.env.MEAL_SERVICE_PORT,
+},
 };
 
 export default nextConfig;
