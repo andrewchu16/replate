335d166528cd0ae5e852e79cb679d9b59fcc8028
Kevin Peng <102827400+pengkev@users.noreply.github.com>
2025-03-22 20:57:19 -0400

    Adding more restaurants to testData.py

--- ai-integration/testData.py
+++ ai-integration/testData.py
@@ -98,7 +98,7 @@ restaurants = [
     "storeName": "Bar Raval",
     "imgUrl": "https://hipfoodiemom.com/wp-content/uploads/2015/11/seafood-paella_horizontal_0967.jpg",
     "distance": 0.4
-  }
+  },
   {
     "name": "Surprise Bag (Pinto Market)",
     "price": 6.99,
@@ -129,4 +129,104 @@ restaurants = [
     "imgUrl": "https://display.blogto.com/listings/6a9a-2012913-ali-babas-dundas.jpg?width=800&quality=70",
     "distance": 0.6
   },
+  {
+    "name": "Tacos",
+    "price": 3.99,
+    "longitude": -79.385,
+    "latitude": 43.654,
+    "rating": 4.6,
+    "storeName": "Taco Bell",
+    "imgUrl": "https://dam.tacobell.com/transform/208b4333-cd1b-4d2b-becd-6e08ef8f90e8/vertical-bucket-2024-E05-CravingsValueMenu-Desktop-jpg",
+    "distance": 0.7
+  },
+  {
+    "name": "Burrito",
+    "price": 7.99,
+    "longitude": -79.386,
+    "latitude": 43.655,
+    "rating": 4.4,
+    "storeName": "Chipotle",
+    "imgUrl": "https://www.reuters.com/resizer/v2/JVXXBGYYDJIJNK54QES4EQRWWE.jpg?auth=71d0ab7c9708e2be1c8d4c72223f2404e3531a44bd67f0da014bcb08ac175b90&width=7875&quality=80",
+    "distance": 0.8
+  },
+  {
+    "name": "Pizza Slice",
+    "price": 2.99,
+    "longitude": -79.387,
+    "latitude": 43.656,
+    "rating": 4.2,
+    "storeName": "Pizza Pizza",
+    "imgUrl": "https://images.happycow.net/venues/1024/32/66/hcmp326613_1880984.jpeg",
+    "distance": 0.9
+  },
+  {
+    "name": "Chicken Sandwich",
+    "price": 6.99,
+    "longitude": -79.388,
+    "latitude": 43.657,
+    "rating": 4.0,
+    "storeName": "Popeyes",
+    "imgUrl": "https://cdn.prod.website-files.com/62e81a6c2171bd40143e605c/62f29607576df97115877cee_Popeyes__Canada_The_Wait_is_Over___Popeyes__Brings_the_Chicken_S.jpeg",
+    "distance": 1.0
+  },
+  {
+    "name": "Big Mac",
+    "price": 4.99,
+    "longitude": -79.389,
+    "latitude": 43.658,
+    "rating": 3.8,
+    "storeName": "McDonald's",
+    "imgUrl": "https://www.foodandwine.com/thmb/8N5jLutuTK4TDzpDkhMfdaHLZxI=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/McDonalds-Hacks-Menu-FT-1-BLOG0122-4ac9d62f6c9143be8da3d0a8553348b0.jpg",
+    "distance": 1.1
+  },
+  {
+    "name": "Hot Dog",
+    "price": 1.99,
+    "longitude": -79.390,
+    "latitude": 43.659,
+    "rating": 5.0,
+    "storeName": "Mama's Best Hot Dog Stand",
+    "imgUrl": "https://cdn-s3.thevarsity.ca/2021/10/BUSINESS-Stefans_hot_dogs_and_Mamas_best-JESSE_MCDOUGALLTHE_VARSITY-web-470x313.jpg",
+    "distance": 1.2
+  },
+  {
+    "name": "Falafel",
+    "price": 5.99,
+    "longitude": -79.391,
+    "latitude": 43.660,
+    "rating": 4.8,
+    "storeName": "Ritz Caribbean Foods",
+    "imgUrl": "https://img.p.mapq.st/?url=https://ir.4sqi.net/img/general/original/18659359_IQ2WjCIOzQFoMCEm0xrXpFd6UUzOa2DVgjLfbP62fa0.jpg?w=3840&q=75",
+    "distance": 1.3
+  },
+  {
+    "name": "Gyro",
+    "price": 8.99,
+    "longitude": -79.392,
+    "latitude": 43.661,
+    "rating": 4.5,
+    "storeName": "The Greek Grill",
+    "imgUrl": "https://thegreekfoodie.com/wp-content/uploads/2025/02/Chicken_Gyros_SQ.jpg",
+    "distance": 1.4
+  },
+  {
+    "name": "Pad Thai",
+    "price": 9.99,
+    "longitude": -79.393,
+    "latitude": 43.662,
+    "rating": 4.3,
+    "storeName": "Thai Express",
+    "imgUrl": "https://tastythriftytimely.com/wp-content/uploads/2023/01/Homemade-Vegan-Pad-Thai-Featured.jpg",
+    "distance": 1.5
+  },
+  {
+    "name": "Butter Chicken",
+    "price": 10.99,
+    "longitude": -79.394,
+    "latitude": 43.663,
+    "rating": 4.1,
+    "storeName": "Madras Curry",
+    "imgUrl": "https://img.taste.com.au/qPAElV3h/taste/2013/07/easy-butter-chicken-image1-197844-1.jpg",
+    "distance": 1.6
+  }
 ]
