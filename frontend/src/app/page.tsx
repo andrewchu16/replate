d8da52580b95c4065755d3a92368ed85adbc2bb8
wqba <wabarurse@gmail.com>
2025-03-22 20:01:52 -0400

    modified landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,29 +1,41 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logoImage from "@/app/assets/images/logo.png"
+import { TbAmpersand } from "react-icons/tb";
+import "./landingPage.css"
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white p-4">
-      <div className="max-w-md w-full text-center space-y-8">
-        <h1 className="text-4xl font-bold text-gray-900">
-          Find Your Perfect Meal
-        </h1>
-        
-        <p className="text-lg text-gray-600">
-          Tell us your preferences and we&apos;ll help you discover the perfect meal nearby.
-        </p>
+    <div className="landing-page-bg min-h-screen flex flex-col items-center justify-center p-4">
+      <div className="max-w-lg h-auto space-y-8 p-8 rounded-lg bg-[#f5fff6] shadow-lg">
+        <div className="flex flex-col space-y-8">
+          <div className="flex flex-row justify-between items-center">
+            <h1 className="text-6xl font-bold text-gray-900">
+              replate
+            </h1>
+            <Image className="hover:scale-110 transition-all" src={logoImage} alt="logo" width={100} height={100} />
+          </div>
 
-        <div className="space-y-4">
+          <h2 className="text-lg text-gray-600">
+            Utilizes the Too Good To Go API to deliver personalized, sustainable meal recommendations based on user preferences.
+          </h2>
+
+          <div className="flex flex-row space-x-4 items-center">
+            <div className="h-auto rounded-lg p-4 shadow-lg max-w-full w-1/2 break-words">
+              To help people in a financial crisis.
+            </div>
+            <TbAmpersand size={48}/>
+            <div className="h-auto rounded-lg p-4 shadow-lg max-w-full w-1/2 break-words">
+              To minimize food waste.
+            </div>
+          </div>
+
           <Link 
             href="/preferences"
-            className="block w-full px-6 py-3 rounded-lg bg-green-500 text-white font-medium 
-              hover:bg-green-600 transition-colors duration-200"
-          >
+            className="block w-full px-6 py-3 rounded-lg bg-[#aedab2] hover:scale-105 transition-all duration-200 text-center font-bold">
             Get Started
           </Link>
-          
-          <p className="text-sm text-gray-500">
-            Quick, easy, and personalized to your taste
-          </p>
         </div>
       </div>
     </div>
